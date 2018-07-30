d81e4e2735c784d0be92de0d071c85ee4ebe694c
AnanDoh <30272848+AnanDoh@users.noreply.github.com>
2018-07-30 15:31:45 -0700

    the js for licence + notes + keywords
    literally the title Q_Q

--- app/static/js/scripts/scripts.js
+++ app/static/js/scripts/scripts.js
@@ -47,6 +47,199 @@ function validateQuery (requiredFields) {
   }
 }
 
+
+function serialiseTextareas (cls) {
+  var values = []
+  $(cls).each(function () {
+    var item = {}
+    if (this.value !== '') {
+      item[$(this).attr('id')] = this.value
+      values.push(item)
+    }
+  })
+  return JSON.stringify(values)
+  }
+
+function cleanup () {
+  const form = jsonifyForm()
+  const newform = {}
+  const exclude = ['licenseName', 'licensePath', 'licenseTitle', 'notesField']
+  // Clone the form values, ommitting empty fields and exclusions
+  $.each(form, function (key, value) {
+    if (value !== '' && value !== [] && $.inArray(key, exclude) === -1) {
+      newform[key] = value
+    }
+  })
+  // Convert comma-separated values to arrays
+  const csvs = ['keywords']
+  for (const property of csvs) {
+    // Only process defined properties
+    if (typeof newform[property] !== 'undefined') {
+      newform[property] = newform[property].trim().split(/\s*,\s*/)
+    }
+  }
+
+  // Convert arrays stored as hidden input string values
+  const arrays = ['notes']
+  for (const property of arrays) {
+    newform[property] = eval(newform[property])
+    // Only process defined properties
+    if (typeof newform[property] !== 'undefined') {
+      const list = []
+      $.each(newform[property], function (key, value) {
+        $.each(value, function (k, v) {
+          list.push(v)
+        })
+      })
+      newform[property] = list
+    }
+  }
+  // Convert objects stored as hidden input string values
+  const objects = ['licenses']
+  for (var property of objects) {
+    newform[property] = eval(newform[property])
+    // Only process defined properties
+    if (typeof newform[property] !== 'undefined') {
+      // Convert evil properties from hidden fields to a list
+      let goodDict = {}
+      let goodList = []
+      let evilProps = []
+      // Get the property keys
+      $.each(newform[property], function (key, value) {
+        $.each(value, function (k, v) {
+          evilProps.push(k)
+        })
+      })
+      // Build object for each ID
+      $.each(evilProps, function (key, item) {
+        item = item.replace(/[a-zA-Z]+/, '')
+        goodDict[item] = {}
+      })
+      // Add the values to the good dict by ID
+      $.each(newform[property], function (i, item) {
+        let obj = newform[property][i]
+        let k = Object.keys(obj)
+        let prop = k[0]
+        let id = prop.replace(/[a-zA-Z]+/, '')
+        let val = item[prop]
+        if (prop.startsWith('licenseName')) {
+          prop = 'name'
+          goodDict[id][prop] = val
+        }
+        if (prop.startsWith('licenseTitle')) {
+          prop = 'title'
+          goodDict[id][prop] = val
+        }
+        if (prop.startsWith('licensePath')) {
+          prop = 'path'
+          goodDict[id][prop] = val
+        }
+      })
+      // Convert any license names to strings and add the good_dict values to the list
+      $.each(goodDict, function (k, v) {
+        if (goodDict[k].length === 1) {
+          v = goodDict[k]['name']
+        } else {
+          goodList.push(v)
+        }
+      })
+      newform[property] = goodList
+    }
+  }
+  return newform
+}
+  $(document).on('click', '.add-license', function () {
+    // Keep count of the number of fields in session storage
+    if ('licenseCount' in sessionStorage) {
+      var count = parseInt(sessionStorage.getItem('licenseCount')) + 1
+      sessionStorage.setItem('licenseCount', count.toString())
+    } else {
+      count = 0
+      sessionStorage.setItem('licenseCount', '0')
+    }
+    // Show the remove icon
+    $(this).next().removeClass('hidden')
+    // Clone the template
+    var $template = $('#licenses-template').clone()
+    $(this).closest('.row').after($template.html())
+    $('.licenseName').last().attr('id', 'licenseName' + count).removeClass('.licenseName')
+    $('.licensePath').last().attr('id', 'licensePath' + count).removeClass('.licensePath')
+    $('.licenseTitle').last().attr('id', 'licenseTitle' + count).removeClass('.licenseTitle')
+    // Serialise the textareas and save the string to the hidden licenses field
+    var serialisedTextareas = serialiseTextareas('.license-field')
+    $('#licenses').val(serialisedTextareas)
+    // console.log($('#licenses').val())
+  })
+
+  $(document).on('click', '.remove-license', function () {
+    // If the field to remove is the only one, clone a new one
+    // NB. There are three sub-fields
+    if ($('.license-field').length === 3) {
+      var count = parseInt(sessionStorage.getItem('licenseCount')) + 1
+      sessionStorage.setItem('licenseCount', count.toString())
+      var $template = $('#licenses-template').clone()
+      $(this).closest('.row').after($template.html())
+      $('.licenseName').last().attr('id', 'licenseName' + count).removeClass('.licenseName')
+      $('.licensePath').last().attr('id', 'licensePath' + count).removeClass('.licensePath')
+      $('.licenseTitle').last().attr('id', 'licenseTitle' + count).removeClass('.licenseTitle')
+    }
+    // Remove the license field
+    $(this).parent().parent().remove()
+    // Display the "licenses" label
+    $('.license-field').eq(0).parent().prev().text('licenses')
+    // Serialise the textareas and save the string to the hidden licenses field
+    var serialisedTextareas = serialiseTextareas('.license-field')
+    $('#licenses').val(serialisedTextareas)
+    // console.log($('#licenses').val())
+  })
+  //can probably give all
+  $(document).on('blur', '.license-field', function () {
+    // Re-serialise the text areas when the user clicks on another element
+    var serialisedTextareas = serialiseTextareas('.license-field')
+    $('#licenses').val(serialisedTextareas)
+    // console.log($('#licenses').val())
+  })
+
+
+  $(document).on('click', '.add-note', function () {
+    if ('noteCount' in sessionStorage) {
+      var count = parseInt(sessionStorage.getItem('noteCount')) + 1
+      sessionStorage.setItem('noteCount', count.toString())
+    } else {
+      count = 0
+      sessionStorage.setItem('noteCount', '0')
+    }
+    $(this).next().removeClass('hidden')
+    var $template = $('#notes-template').clone()
+    $(this).closest('.row').after($template.html())
+    $('.note-field').last().attr('id', 'note' + count)
+    var serialisedTextareas = serialiseTextareas('.note-field')
+    $('#notes').val(serialisedTextareas)
+    // console.log($('#notes').val())
+  })
+
+  $(document).on('click', '.remove-note', function () {
+    if ($('.note-field').length === 1) {
+      var count = parseInt(sessionStorage.getItem('noteCount')) + 1
+      sessionStorage.setItem('noteCount', count.toString())
+      var $template = $('#notes-template').clone()
+      $(this).closest('.row').after($template.html())
+      $('.note-field').last().attr('id', 'note' + count)
+    }
+    $(this).parent().parent().remove()
+    $('.note-field').eq(0).parent().prev().text('notes')
+    var serialisedTextareas = serialiseTextareas('.note-field')
+    $('#notes').val(serialisedTextareas)
+    // console.log($('#notes').val())
+  })
+
+  $(document).on('blur', '.note-field', function () {
+    var serialisedTextareas = serialiseTextareas('.note-field')
+    $('#notes').val(serialisedTextareas)
+    // console.log($('#notes').val())
+  })
+  // End Property Cloning
+
 //
 // Script Methods
 //
@@ -238,7 +431,7 @@ $(document).ready(function () {
     e.preventDefault()
     $('form').hide()
     $('#previewDisplay').show()
-    var jsonform = jsonifyForm($('#manifestForm'))
+    var jsonform = cleanup()
     $('#manifest').html(JSON.stringify(jsonform, null, '  '))
   })
   $('#hide').click(function (e) {
