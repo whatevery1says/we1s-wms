e20f2ac08843901731f4996fd965041f2197d51a
scottkleinman <scott.kleinman@csun.edu>
2018-12-08 17:47:45 -0800

    Fix pycodestyle violations

--- app/__init__.py
+++ app/__init__.py
@@ -111,6 +111,7 @@ def not_found_error(error):
     """Load 404 Error page."""
     return render_template('errors/404.html'), 404
 
+
 @app.template_filter('is_list')
 def is_list(value):
     """
@@ -119,6 +120,7 @@ def is_list(value):
     Usage: `{% if var|is_list %}`
     """
     return isinstance(value, list)
+
 
 # ----------------------------------------------------------------------------#
 # Launch.
