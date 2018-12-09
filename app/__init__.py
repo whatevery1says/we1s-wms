e2b8302df9e1a85bc4cdaab512f9514fdbc46664
scottkleinman <scott.kleinman@csun.edu>
2018-12-08 17:37:30 -0800

    Comment out pointless string statement

--- app/__init__.py
+++ app/__init__.py
@@ -127,8 +127,6 @@ def is_list(value):
 if __name__ == '__main__':
     app.run()
 # Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
