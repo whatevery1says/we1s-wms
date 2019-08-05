94353862c74877805126717f04070a6636026e5e
scottkleinman <scott.kleinman@csun.edu>
2019-08-05 12:38:25 -0700

    Comment out old code

--- app/sources/helpers/methods.py
+++ app/sources/helpers/methods.py
@@ -19,9 +19,12 @@ from pymongo import MongoClient
 
 
 # Set up the MongoDB client, configure the databases, and assign variables to the "collections"
-client = MongoClient('mongodb://localhost:27017')
-db = client.we1s
-sources_db = db.Sources
+# client = MongoClient('mongodb://localhost:27017')
+# db = client.we1s
+# sources_db = db.Sources
+client = MongoClient('mongodb://mongo:27017')
+# DB has one collection, so treat it as the whole DB
+sources_db = client.Sources.Sources
 
 # ----------------------------------------------------------------------------#
 # General Helper Functions
